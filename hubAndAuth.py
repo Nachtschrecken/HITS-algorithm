0793f9168ca41fa02f502f1f1662810b4eb7b1e4
Nachtschrecken <ferris@uldagon.com>
2022-11-02 13:48:35 +0100

    finished HITS algorithm

--- hubAndAuth.py
+++ hubAndAuth.py
@@ -1,14 +1,27 @@
 import numpy as np
-from sklearn import preprocessing
+from sklearn.preprocessing import normalize
 
-l = np.matrix('0 1 1 1 0; 1 0 0 1 0; 0 0 0 0 1; 0 1 1 0 0; 0 0 0 0 0')
+# Input web Matrix
+l = np.matrix('0.0 1.0 1.0 1.0 0.0; 1.0 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 0.0 1.0; 0.0 1.0 1.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0')
+
+# Default matrices for calculation, do not alter
+np.set_printoptions(precision=3)
 lt = l.transpose()
-h = np.matrix('1; 1; 1; 1; 1')
+h = np.matrix('1.0; 1.0; 1.0; 1.0; 1.0')
+a = np.matrix('1.0; 1.0; 1.0; 1.0; 1.0')
 
+# Computation for both hubiness and authority
 def compute(h):
     a = np.matmul(lt,h)
-    a = preprocessing.minmax_scale(a, feature_range=(0, 1), axis=0, copy=True)
-    return np.matmul(l,a)
+    a *= np.matrix(1.0/a.max())
+    h = np.matmul(l,a)
+    h *= np.matrix(1.0/h.max())
+    return a,h
 
-for x in range(2):
-    h = compute(h)
+# Iterate long enough for acceptable limits
+for x in range(1000):
+    a,h = compute(h)
+
+# Print results
+print("Hubiness:\n",h)
+print("Authority:\n",a)
